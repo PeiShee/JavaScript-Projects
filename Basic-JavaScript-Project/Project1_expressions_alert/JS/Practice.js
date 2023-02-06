c843c8d56a0d2b0ed1387f52b9d3b0f002ff8acf
PeiShee <109766844+PeiShee@users.noreply.github.com>
2023-02-06 14:20:57 +0000

    JS Assignment 1 completed
    JavaScript Module 1 Assignment 1

---
+++ Basic-JavaScript-Project/Project1_expressions_alert/JS/Practice.js
@@ -0,0 +1,35 @@
+alert('Hello, World!'); //two method to create alaert window.
+window.alert('Hello, world!'); //two method to create alaert window.
+document.write('Hello, world!'); //written text method 1
+var A = 'This is a string.'; // written text method 2
+document.write(A);
+
+let answer1 = 'correct'; //variable code
+let answer2 = 'wrong';
+let x = 'answer1';
+alert(x);
+
+
+var C = 'concatenated'+'string'; //concatenation
+document.write(C)
+
+//Multiple variable with different font colour
+var D = 'January';
+var D =  D.fontcolor('red');
+
+var E = 'February';
+var E =  E.fontcolor('purple');
+
+var F = 'March';
+var F =  F.fontcolor('black');
+document.write(D)
+
+var blues = 'I have the blues';
+var blues = blues.fontcolor('blues');
+document.write(blues);
+
+//statement and expression
+document.write(3+3);
+
+//challenge 2 
+id ='demo'
--- Basic-JavaScript-Project/Project1_expressions_alert/JS/main.js
+++ Basic-JavaScript-Project/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,23 @@
-alert('Hello, World!');
+window.alert('Welcome!'); //Create alert window
+document.write('This is the basic JavaScript introduction practice.'); //written text 
+
+//variable with concatenation
+var A = 'I am ' ;
+var B = 'Pei Shee. Written in ';
+var C = A+B
+document.write(C)
+
+
+//Multiple variable with different font colour
+var D = 'January';
+var D =  D.fontcolor('red');
+
+var E = 'February';
+var E =  E.fontcolor('purple');
+
+var F = 'March';
+var F =  F.fontcolor('black');
+document.write(E)
+
+//statement and expression
+document.write(2022+1);
