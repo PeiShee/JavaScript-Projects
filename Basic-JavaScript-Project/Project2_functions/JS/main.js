e90548284b2332a170ec33c70cf756de4293eda3
PeiShee <109766844+PeiShee@users.noreply.github.com>
2023-02-06 17:43:32 +0000

    Assignment 2
    Module 1 assignment 2 += operator

---
+++ Basic-JavaScript-Project/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+//+= operator for concatenate.
+function myFunction () {
+    var sentence = "I am learning";
+    sentence += "a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
