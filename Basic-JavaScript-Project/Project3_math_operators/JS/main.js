48dfe1b21098c2d85d03273289d81d76e253aa0a
PeiShee <109766844+PeiShee@users.noreply.github.com>
2023-02-08 20:37:07 +0000

    Assignment 16 troubleshot
    Assignment 16 troubleshot

--- Basic-JavaScript-Project/Project3_math_operators/JS/main.js
+++ Basic-JavaScript-Project/Project3_math_operators/JS/main.js
@@ -1,41 +1,42 @@
-function addition_Function() {
-    var addition = 2+2;
-    document .getElementById("Math1").innerHTML = "2 + 2= " + addition;
-}
-
-function substraction_Function() {
-    var Substraction  = 2-2;
-    document .getElementById("Math2").innerHTML = "2 - 2 = " + Substraction;
-}
-
-function multiplication() {
-    var simple_Math = 2*2;
-    document .getElementById("Math3").innerHTML = "6 x 8 = " + simple_Math;
-}
-
-function addition_Function() {
-    var simple_Math = 2 / 2;
-    document .getElementById("Math4").innerHTML = "2 / 2 = " + simple_Math;
-}
-
-function addition_Function() {
-    var simple_Math = (2+2) * 2 / 2 -2;
-    document .getElementById("Math5").innerHTML = "2 plus 2, multiplied by 2, divided in 2 and then substracted by 2 equals " + simple_Math;
-}
-
-function modulus_Operator () {
-    var simple_Math = 25 % 6;
-    document .getElementById("Math6").innerHTML = "When you divide 25 bu 6 you have a remainder of: "+ simple_Math;
-}
-
-
-function negation_Operator () {
-    var x = 2;
-    document .getElementById("Math7").innerHTML = -x;
-}
+window.alert(Math.random() * 100);
 
 var x = 5.25;
 x--;
 document.write(x);
 
-window.alert(math.random() * 100);
+function addition_Function() {
+    var addition = 2+2;
+    document.getElementById("Math1").innerHTML = "2 + 2= " + addition;
+}
+
+function substraction_Function() {
+    var Substraction  = 2-2;
+    document.getElementById("Math2").innerHTML = "2 - 2 = " + Substraction;
+}
+
+function multiplication() {
+    var simple_Math = 2*2;
+    document.getElementById("Math3").innerHTML = "6 x 8 = " + simple_Math;
+}
+
+function division_Function() {
+    var simple_Math = 2 / 2;
+    document.getElementById("Math4").innerHTML = "2 / 2 = " + simple_Math;
+}
+
+function simplemath_Function() {
+    var simple_Math = (2+2) * 2 / 2 -2;
+    document.getElementById("Math5").innerHTML = "2 plus 2, multiplied by 2, divided in 2 and then substracted by 2 equals " + simple_Math;
+}
+
+function modulus_Operator () {
+    var simple_Math = 25 % 6;
+    document.getElementById("Math6").innerHTML = "When you divide 25 by 6 you have a remainder of: "+ simple_Math;
+}
+
+
+function negation_Operator () {
+    var x = 2;
+    document.getElementById("Math7").innerHTML = -x;
+}
+
