475d27602b5fd77ddc1e7ef06fcaf435134f70ac
PeiShee <109766844+PeiShee@users.noreply.github.com>
2023-02-09 20:46:39 +0000

    console log
    Assignment 28

--- Basic-JavaScript-Project/Project5_comparison_type_coercion/JS/main.js
+++ Basic-JavaScript-Project/Project5_comparison_type_coercion/JS/main.js
@@ -6,9 +6,21 @@ function my_Function1 () {
 }
 
 function my_Function2 () {
-    document.getElementById("Test2").innerHTML = isNAN('This is a string');
+    document.getElementById("Test2").innerHTML = isNaN('This is a string');
 }
 
 function my_Function3 () {
-    document.getElementById("Test3").innerHTML = isNAN('007');
+    document.getElementById("Test3").innerHTML = isNaN('007');
 }
+
+document.write(2E10);
+document.write(-2E10);
+
+document.write(10 > 2);
+document.write(10 < 2);
+
+document.write("10"+5);
+
+console.log(2 + 2);
+
+
